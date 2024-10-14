f11b8d4724c3fe3edc18166ef1920392b70de9bc
Torsten Wylegala <mail+github@twyleg.de>
2024-10-14 12:30:14 +0200

    Update template_script.sh

--- template_script.sh
+++ template_script.sh
@@ -9,5 +9,8 @@
 # When in DEBUG mode, enable a shell mode where all executed commands are printed to the terminal
 [ "$DEBUG" == 1 ] && set -x
 
+# Exit on command error
+set -e
+
 # Get the dir of the currently running scripts
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
